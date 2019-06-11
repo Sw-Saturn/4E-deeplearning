9b8a41e4e117f6db31c925731a7a4cb621653517
Kanta Demizu <demizu.1203@gmail.com>
2019-06-11 14:39:20 +0900

    [ERR] UnityからのBash呼び出しができない
    Former-commit-id: 7ac6817cd6478c9f545bb959687ccbe50ee1ea14

---
+++ Assets/Scripts/callPython.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.IO;
+using System.Diagnostics;
+using System.Collections;
+
+public class PostProcessBuildProcessor {
+
+    [PostProcessBuild]
+    public static void OnPostProcessBuild(BuildTarget target, string path) {
+        var output = DoBashCommand("echo hello");
+        UnityEngine.Debug.Log(output);
+    }
+
+    static string DoBashCommand(string cmd){
+        var p = new Process();
+        p.StartInfo.FileName = "/bin/bash";
+        p.StartInfo.Arguments = "-c \" " + cmd + " \"";
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.RedirectStandardOutput = true;
+        p.Start();
+
+        var output = p.StandardOutput.ReadToEnd();
+        p.WaitForExit();
+        p.Close();
+
+        return output;
+    }
+}
