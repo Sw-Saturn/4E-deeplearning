d2ed29bc6f25dc04162fec367953f637e4bef6a6
Kanta Demizu <demizu.1203@gmail.com>
2019-07-02 13:33:43 +0900

    [ADD] APIサーバが動作するように
    Former-commit-id: e0677a10794d9dbc3bc5b3e528b72b3b3f5cfa8b

--- electronic-device-detection-api/app.py
+++ electronic-device-detection-api/app.py
@@ -1,59 +1,79 @@
-from flask import Flask, render_template, request
-import subprocess,os,json
+from flask import Flask, request, jsonify
+import os
+import base64
 from label_image import *
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('api_index.html')
+def convert_b64_to_file(b64, outfile_path):
+    """
+    b64をデコードしてファイルに書き込む
+    """
+    s = base64.decodebytes(b64)
+    with open(outfile_path,"wb") as f:
+        f.write(s)
 
 
-@app.route('/result', methods=['GET','POST'])
+@app.route('/', methods=['POST'])
 def result():
-    # submitした画像が存在したら処理する
-    if request.files['image']:
-        request.files['image'].save('./temp.jpg')
+    # Bad request
+    if not request.headers['Content-Type'] == 'application/json':
+        return jsonify(res='failure'), 400
+    # jsonはdict型なので即変換できないからlistに入れて処理している
 
-        load_graph('./retrained_graph.pb')
-        label_file = "retrained_labels.txt"
-        input_height = 299
-        input_width = 299
-        input_mean = 0
-        input_std = 255
-        input_layer = "Placeholder"
-        output_layer = "final_result"
+    # jsonを取得
+    data = request.json
+    # keysを取得
+    keys_array = list(data.keys())
+    # valuesを取得
+    values_array = list(data.values())
+    """
+    送ってくるjsonは一つ目の要素が{画像名:base64エンコード}としたもの
+    """
 
-        graph = load_graph('./retrained_graph.pb')
-        t = read_tensor_from_image_file(
-            './temp.jpg',
-            input_height=input_height,
-            input_width=input_width,
-            input_mean=input_mean,
-            input_std=input_std)
+    # 画像の保存名
+    save_name = keys_array[0] + ".jpg"
+    # コンバート
+    convert_b64_to_file(bytes(values_array[0], "utf-8"), save_name)
 
-        input_name = "import/" + input_layer
-        output_name = "import/" + output_layer
-        input_operation = graph.get_operation_by_name(input_name)
-        output_operation = graph.get_operation_by_name(output_name)
+    load_graph('./retrained_graph.pb')
+    label_file = "retrained_labels.txt"
+    input_height = 299
+    input_width = 299
+    input_mean = 0
+    input_std = 255
+    input_layer = "Placeholder"
+    output_layer = "final_result"
 
-        with tf.Session(graph=graph) as sess:
-            results = sess.run(output_operation.outputs[0], {
-                input_operation.outputs[0]: t
-            })
-        results = np.squeeze(results)
+    graph = load_graph('./retrained_graph.pb')
+    t = read_tensor_from_image_file(
+        './temp.jpg',
+        input_height=input_height,
+        input_width=input_width,
+        input_mean=input_mean,
+        input_std=input_std)
 
-        top_k = results.argsort()[-5:][::-1]
-        labels = load_labels(label_file)
-        final_dict = {}
-        for i in top_k:
-            print(labels[i], results[i])
-            final_dict[labels[i]] = str(results[i])
+    input_name = "import/" + input_layer
+    output_name = "import/" + output_layer
+    input_operation = graph.get_operation_by_name(input_name)
+    output_operation = graph.get_operation_by_name(output_name)
 
-        # render_template('./result.html')
-        return render_template('result.html', title='予想クラス', predict_Confidence=final_dict)
+    with tf.Session(graph=graph) as sess:
+        results = sess.run(output_operation.outputs[0], {
+            input_operation.outputs[0]: t
+        })
+    results = np.squeeze(results)
+
+    top_k = results.argsort()[-5:][::-1]
+    labels = load_labels(label_file)
+    final_dict = {}
+    for i in top_k:
+        print(labels[i], results[i])
+        final_dict[labels[i]] = str(results[i])
+
+    return jsonify(final_dict, response='success')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
---
+++ electronic-device-detection-api/test_post.py
@@ -0,0 +1,38 @@
+import urllib.request
+import json
+import base64
+
+
+def convert_b64(file_path):
+    """
+    b64にエンコード
+    """
+    return base64.encodebytes(open(file_path, 'rb').read()).decode("utf-8")
+
+
+if __name__ == '__main__':
+    # 送信先
+    # post_request
+    url = "http://0.0.0.0:5000/"
+    # 画像ファイル
+    image = "../face.jpg"
+    # 画像の名前
+    json_key = "temp"
+
+    method = "POST"
+    headers = {"Content-Type": "application/json"}
+    # エンコードしたもの
+
+    value = convert_b64(image)
+    # json_key:名前
+    # value:中身
+
+    # PythonオブジェクトをJSONに変換する
+    obj = {json_key: value}
+    # json_data:認識結果などのjsonを入れてもOK
+    json_data = json.dumps(obj).encode("utf-8")
+    # httpリクエストを準備してPOST
+    request = urllib.request.Request(url, data=json_data, method=method, headers=headers)
+    with urllib.request.urlopen(request) as response:
+        response_body = response.read().decode("utf-8")
+        print(response_body)
